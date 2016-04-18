b32413e3f80da9dea27b52e8e3e113c860efd178
ManuLdb <emmanuellorenzoldb@gmail.com>
2016-04-18 18:35:41 +0200

    Mise en place des div penché faut que je retouche la dernière

--- index.php
+++ index.php
@@ -68,12 +68,13 @@
 <a class="cd-nav-trigger cd-img-replace">Open navigation<span></span></a>
 
 <section id="section1" class="cd-section">
-
+    <div class="triangledown"></div>
+    <div class="triangledownbordure"></div>
+    <p class="triangledowntext">UN TEXTE</p>
     <a href="#section2" class="cd-scroll-down cd-img-replace">scroll down</a>
 </section><!-- cd-section -->
-
 <section id="section2" class="cd-section">
-    <p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto numquam, totam iusto officia earum perferendis, culpa ad atque eveniet praesentium nobis expedita similique beatae tenetur. Distinctio vel tenetur, id cum.&rdquo;</p>
+
 </section><!-- cd-section -->
 
 <section id="section3" class="cd-section">
