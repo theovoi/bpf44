3cc6921ec656044c3f50772021ac488a987816a1
Theov <theo.voison@gmail.com>
2016-04-23 19:01:25 +0200

    add favicon + début de SEO

--- index.php
+++ index.php
@@ -5,10 +5,32 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Bienvenue sur le site du Briollay Pop Festival. Freaks Music & Sun le temps d'un weekend">
+        <meta name="keywords" content="briollay,pop,festival,musique,psychédélique,ds,éléctro,Angers,freaks,des,champs">
+
         <title>Briollay Pop Festival #4</title>
 
+        <!-- FAVICON -->
+        <link rel="apple-touch-icon" sizes="57x57" href="favicon.ico/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="favicon.ico/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="favicon.ico/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="favicon.ico/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="favicon.ico/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="favicon.ico/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="favicon.ico/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="favicon.ico/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon.ico/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="favicon.ico/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="favicon.ico/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon.ico/favicon-16x16.png">
+        <link rel="manifest" href="favicon.ico/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
         <!-- Open Graph -->
-        <meta property="og:title" content="Briollay Pop Festival #4"/>
+        <meta property="og:title" content="Briollay Pop Festival"/>
         <meta property="og:type" content="website"/>
         <meta property="og:description" content="Bienvenue sur le site du Briollay Festival #4"/>
         <meta property="og:url" content=""/>
